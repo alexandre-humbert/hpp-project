e4a9f07a2d232d0755edda2f5474ab91423327b2
Humbert Alexandre <alex.hum@hotmail.fr>
2021-05-05 15:36:18 +0200

    scan files

--- src/main/java/corona/Reader.java
+++ src/main/java/corona/Reader.java
@@ -15,9 +15,9 @@ public class Reader {
 	private static BlockingQueue<String[]> queue;
 	private static int next_id = 0;
 
-	public Reader(String chemin, BlockingQueue q) throws FileNotFoundException {
+	public Reader(String chemin,BlockingQueue q) throws FileNotFoundException {
 		bf = new BufferedReader(new FileReader(chemin));
-		queue = q;
+		queue=q;
 	}
 
 	public String[] getNextLine() {
@@ -36,16 +36,39 @@ public class Reader {
 		}
 	}
 
+	public static boolean notNull(String[][] array) {
+		for (String[] element : array) {
+			if(element!=null) return true; break;
+		}
+		return false;
+	}
 	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-		String[][] all_data = { { "-1" }, { "-1" }, { "-1" } };
-		String[] used_data = new String[4];
+		String path  = "src\\main\\resources\\20\\";
 		queue = new ArrayBlockingQueue(1000);
-		Reader[] naiv = { new Reader("src\\main\\resources\\20\\France.csv", queue),
-				new Reader("src\\main\\resources\\20\\Italy.csv", queue),
-				new Reader("src\\main\\resources\\20\\Spain.csv", queue) };
+		int nbfiles=0;
+		File[] files = null;
+        try {
+            File f = new File(path);
+            files = f.listFiles();
+            nbfiles = files.length;
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+		
+		Reader[] naiv  = new Reader[nbfiles];
+		String[] used_data= new String[4];
+		String[][] all_data = new String[nbfiles][];
+		String[] all_data_init = { "-1" };		
+		for (int i=0; i < nbfiles;i++) {
+			
+		naiv[i] = new Reader(path+ files[i].getName(), queue);
+			all_data[i] = all_data_init;
+		}
+		
 		double minTimestamp = Double.POSITIVE_INFINITY;
 		int min = 0;
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < nbfiles; i++) {
 			all_data[i] = naiv[i].getNextLine();
 			if (Double.parseDouble(all_data[i][4]) < minTimestamp) {
 				minTimestamp = Double.parseDouble(all_data[i][4]);
@@ -58,10 +81,10 @@ public class Reader {
 		used_data[3] = "France";
 		queue.put(used_data);
 		all_data[min] = naiv[min].getNextLine();
-		while (all_data[0] != null || all_data[1] != null || all_data[2] != null) {
+		while (notNull(all_data)) {
 			minTimestamp = Double.POSITIVE_INFINITY;
 
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < nbfiles; i++) {
 				if (all_data[i] != null && Double.parseDouble(all_data[i][4]) < minTimestamp) {
 					minTimestamp = Double.parseDouble(all_data[i][4]);
 					min = i;
@@ -71,7 +94,7 @@ public class Reader {
 			used_data[1] = all_data[min][4];
 			used_data[2] = all_data[min][5];
 			used_data[3] = "France";
-			// System.out.println(used_data[0]+" "+used_data[1]);
+			//System.out.println(used_data[0]+" "+used_data[1]);
 			queue.put(used_data);
 			all_data[min] = naiv[min].getNextLine();
 		}
