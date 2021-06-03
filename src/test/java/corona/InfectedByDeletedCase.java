d57bba435b70db0adfa1c4fa19fa01d085c14080
Humbert Alexandre <alex.hum@hotmail.fr>
2021-06-03 12:24:08 +0200

    Add files via upload

---
+++ src/test/java/corona/InfectedByDeletedCase.java
@@ -0,0 +1,14 @@
+package corona;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InfectedByDeletedCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/test/java/corona/ReaderTest.java
+++ src/test/java/corona/ReaderTest.java
@@ -1,8 +1,10 @@
 package corona;
 
 import static org.junit.Assert.*;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -12,11 +14,15 @@ import org.junit.Test;
 public class ReaderTest {
 
 	@Test
-	public void test() throws InterruptedException {
+	public void test() throws InterruptedException, IOException {
 		BlockingQueue<String[]> ReaderQueue;
 		ReaderQueue = new ArrayBlockingQueue<String[]>(100);
 		Reader rd;
+		BufferedReader bf = null;
+	    String line;
+	    String value;
 		try {
+			bf = new BufferedReader(new FileReader("src\\test\\resources\\Example_result\\test_reader.txt"));
 			rd = new Reader("src\\test\\resources\\Example\\",ReaderQueue);
 			Thread ReaderThread =new Thread(rd);  
 			ReaderThread.start();
@@ -25,7 +31,11 @@ public class ReaderTest {
 			System.out.println("Size " + ReaderQueue.size());
 			int size = ReaderQueue.size();
 			for (int i=0; i<size; i++) {
-				System.out.println(ReaderQueue.take()[0]);
+				line = bf.readLine();
+				value = Arrays.toString(ReaderQueue.take());
+				if (line.equals(value) != true) {
+					fail("Does not match");
+				}
 			}
 			
 		} catch (FileNotFoundException e) {
